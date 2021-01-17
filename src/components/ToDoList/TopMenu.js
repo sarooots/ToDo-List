b5a4c638b897d538d78ae7cb56924589f0fb067c
sarooots <hayk.mkrtchyan.96.oct@gmail.com>
2021-01-17 19:29:12 +0400

    additional

--- src/components/ToDoList/TopMenu.js
+++ src/components/ToDoList/TopMenu.js
@@ -5,6 +5,7 @@ import classes from "./ToDoList.module.sass"
 import idGenerator from "../../helpers/idGenerator"
 import DeleteSelected from "./DeleteSelected"
 import PropTypes from "prop-types"
+import TaskEditor from "./TaskEditor"
 
 class TopMenu extends Component {
     static propTypes = {
@@ -17,67 +18,12 @@ class TopMenu extends Component {
         deselect: PropTypes.func.isRequired,
     }
 
-    state = {
-        name: '',
-        desc: '',
-        deadline: new Date().getTime() + 1440 * 60 * 1000,
-        status: this.props.status[1],
-        editMode: false,
-        _id: idGenerator()
-    }
-    changeTask = (event, property ) => {
-         new Date(event.target.value).getTime()
-        const  value = property === 'deadline'? new Date(event.target.value).getTime(): event.target.value
-        this.setState({[property]: value})
-    }
-    handleInputKeyPress = event => {
-        if (event.key === 'Enter') {
-            const newTask = {...this.state}
-            this.props.addTask(newTask)
-            this.setState({name: '', desc: ''})}
-    }
 
     render() {
         const {addTask, tasks, selectedTasks, removeSelected, selectAllTasks, deselect} = this.props
 
         return (
             <Form onSubmit={(event)=> event.preventDefault()}>
-                <Form.Row >
-                    <Form.Group as={Col} controlId='taskName'>
-                        <Form.Label>Task Name</Form.Label>
-                        <Form.Control type="text"
-                                      placeholder="Add new task"
-                                      value={this.state.name}
-                                      className={classes.input}
-                                      onKeyPress={(event) => this.handleInputKeyPress(event)}
-                                      onChange={(event) => {this.changeTask(event, 'name')}}
-                                      disabled={!!selectedTasks.size}
-                        />
-                    </Form.Group>
-                    <Form.Group as={Col} controlId='deadline'>
-                        <Form.Label>Deadline</Form.Label>
-                        <Form.Control type="datetime-local"
-                                      placeholder="Write task description"
-                                      format='timestamp'
-                                      min={moment(this.state.deadline).format('YYYY-MM-DDThh:mm')}
-                                      value={moment(this.state.deadline).format('YYYY-MM-DDThh:mm')}
-                                      onKeyPress={(event) => this.handleInputKeyPress(event)}
-                                      onChange={(event) => {this.changeTask(event, 'deadline')}}
-                                      disabled={!!selectedTasks.size}
-                        />
-                    </Form.Group>
-
-                </Form.Row>
-                <Form.Row >
-                    <Form.Group as={Col} controlId='taskDesc'>
-                        <Form.Control as="textarea"
-                                      placeholder="Write task description"
-                                      value={this.state.desc}
-                                      onChange={(event) => {this.changeTask(event, 'desc')}}
-                                      disabled={!!selectedTasks.size}
-                        />
-                    </Form.Group>
-                </Form.Row>
                 <Form.Row className="align-items-center justify-content-md-center">
                     <Col xs="auto">
                         <label className={classes.select}>
@@ -102,6 +48,12 @@ class TopMenu extends Component {
                     </Col>
                     <Col xs="auto">
                         <DeleteSelected
+                            removeSelected={removeSelected}
+                            selectedTasks={selectedTasks}
+                        />
+                    </Col>
+                    <Col xs="auto">
+                        <TaskEditor
                             removeSelected={removeSelected}
                             selectedTasks={selectedTasks}
                         />
