4fd11ae0d576bfe9a2c2dab93e9668e379ed0860
sarooots <hayk.mkrtchyan.96.oct@gmail.com>
2021-03-10 21:36:19 +0400

    34 redux-logger turned off in production environment

--- src/store/store.js
+++ src/store/store.js
@@ -3,5 +3,12 @@ import reducer from "./reducer";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 
-const middleware = applyMiddleware(thunk, logger)
-export const store = createStore(reducer, middleware)
+console.log(process.env.NODE_ENV)
+const params = [thunk]
+if (process.env.NODE_ENV==="development"){
+    params.push(logger)
+}
+
+
+const middleware =  applyMiddleware(...params)
+export const store =  createStore(reducer, middleware)
